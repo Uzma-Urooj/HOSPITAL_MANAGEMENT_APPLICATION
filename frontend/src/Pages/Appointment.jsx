cf9477de6eee1e68005b9d03f9d32b0baa4b1123
Uzma <uzmaurooj41@gmail.com>
2025-07-03 11:30:42 +0530

    Re-added frontend folder as normal directory

---
+++ frontend/src/Pages/Appointment.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from "../Components/Hero";
+import AppointmentForm from "../Components/AppointmentForm";
+
+
+const Appointment = () => {
+  return (
+    <>
+       <Hero
+        title={"Schedule Your Appointment | ZeeCare Medical Institute"}
+        imageUrl={"/signin.png"}
+      />
+      <AppointmentForm/>
+    </>
+  )
+}
+
+export default Appointment
